90b1791bcf4e20609ed52c928e70152c0757ed2b
Siju Jacob <sijujacob@Sijus-MacBook-Pro-2.local>
2021-11-23 16:59:28 -0500

    aws-nodejs-curd first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const productRouter = require('./routes/product');
+app.use(express.json());
+app.use('/product', productRouter);
+
+app.get('/', (req, res) => {
+    res.send('<h2>WELCOME - AWS-NODE-CURD App </h2>')
+});
+app.get('/health', (req, res) => {
+    res.send()
+});
+
+app.listen(port, () => {
+    console.log('YEYY!!! AWS-NODE-CURD App is UP and LISTENING to port : ', port);
+})
